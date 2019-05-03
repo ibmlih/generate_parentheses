a6a568c002c620f64ffc074743d689d3217932e6
Eunsub Lee <petlee@ucdavis.edu>
2019-05-03 03:14:27 -0700

    Final

--- generateParentheses.py
+++ generateParentheses.py
@@ -1,16 +1,16 @@
 def generateParenthesis(N):
     results = []
 
-    def backtrack(parentheses, opening, closing):
-        if len(parentheses) == 2 * N:
-            results.append(parentheses)
+    def backtrack(parenthesis, opening, closing):
+        if len(parenthesis) == 2 * N:
+            results.append(parenthesis)
             return
 
         if opening < N:
-            backtrack(parentheses+'(', opening + 1, closing)
+            backtrack(parenthesis + '(', opening + 1, closing) # generate opening bracket
 
         if closing < opening:
-            backtrack(parentheses+')', opening, closing + 1)
+            backtrack(parenthesis + ')', opening, closing + 1) # generate closing bracket
 
     backtrack('', 0, 0)
     return results
--- generateParenthesesNaive.py
+++ generateParenthesesNaive.py
@@ -2,15 +2,15 @@ import itertools
 
 def generateParenthesis(N):
     results = []
-    parentheses = '(' * N + ')' * N # N pairs of '(' and ')'
+    parenthesis = '(' * N + ')' * N # N pairs of '(' and ')'
 
-    # Generate all permuations of parentheses
-    for p in set(itertools.permutations(parentheses, N*2)):
+    # Generate all permuations of parenthesis
+    for p in set(itertools.permutations(parenthesis, N*2)):
         balance = 0
 
         # Check if the parenthesis is valid
-        for i in p:
-            if i == '(':
+        for s in p:
+            if s == '(':
                 balance += 1
             else:
                 balance -= 1
