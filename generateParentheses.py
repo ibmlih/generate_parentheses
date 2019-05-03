a0958e5ae18fac8c312f4249c515cd5849402831
Eunsub Lee <petlee@ucdavis.edu>
2019-05-03 03:05:55 -0700

    fixed

--- generateParentheses.py
+++ generateParentheses.py
@@ -1,21 +1,16 @@
-import itertools
-
 def generateParenthesis(N):
     results = []
 
-    def backtrack(string, left, right):
-        if len(string) == 2 * N:
-            results.append(string)
+    def backtrack(parentheses, opening, closing):
+        if len(parentheses) == 2 * N:
+            results.append(parentheses)
             return
-        if left < N:
-            backtrack(string+'(', left+1, right)
-        if right < left:
-            backtrack(string+')', left, right+1)
+
+        if opening < N:
+            backtrack(parentheses+'(', opening + 1, closing)
+
+        if closing < opening:
+            backtrack(parentheses+')', opening, closing + 1)
 
     backtrack('', 0, 0)
     return results
-
-
-# print(generateParenthesis(3))
-for s in itertools.combinations([0,0,1,1], 2):
-    print(s)
--- generateParenthesesNaive.py
+++ generateParenthesesNaive.py
@@ -2,34 +2,22 @@ import itertools
 
 def generateParenthesis(N):
     results = []
-    binary = [] # 0 represents '(' and 1 represent ')'
+    parentheses = '(' * N + ')' * N # N pairs of '(' and ')'
 
-    # Initialize N pairs of 0s and 1s
-    for i in range(N):
-        binary.append(0)
-        binary.append(1)
-
-    # Generate all permuations of binary
-    for b in set(itertools.permutations(binary, N*2)):
+    # Generate all permuations of parentheses
+    for p in set(itertools.permutations(parentheses, N*2)):
         balance = 0
 
         # Check if the parenthesis is valid
-        for i in b:
-            if i == 0:
+        for i in p:
+            if i == '(':
                 balance += 1
             else:
                 balance -= 1
             if balance < 0:
                 break
-
-        # Convert binary into parenthesis
+                
         if balance == 0:
-            parenthesis = ''
-            for i in b:
-                if i == 0:
-                    parenthesis += '('
-                else:
-                    parenthesis += ')'
-            results.append(parenthesis)
+            results.append(''.join(p))
             
     return results
