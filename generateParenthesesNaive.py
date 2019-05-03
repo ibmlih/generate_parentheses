bb4a664c7da4b2bd948b17418c5d06d658c2205d
Eunsub Lee <petlee@ucdavis.edu>
2019-05-03 02:42:20 -0700

    Comments

--- generateParenthesesNaive.py
+++ generateParenthesesNaive.py
@@ -2,17 +2,19 @@ import itertools
 
 def generateParenthesis(N):
     results = []
-    binary = [] # 0 represents ( and 1 represent )
+    binary = [] # 0 represents '(' and 1 represent ')'
 
+    # Initialize N pairs of 0s and 1s
     for i in range(N):
         binary.append(0)
         binary.append(1)
 
-    for t in set(itertools.permutations(binary, N*2)):
+    # Generate all permuations of binary
+    for b in set(itertools.permutations(binary, N*2)):
         balance = 0
 
         # Check if the parenthesis is valid
-        for i in t:
+        for i in b:
             if i == 0:
                 balance += 1
             else:
@@ -23,7 +25,7 @@ def generateParenthesis(N):
         # Convert binary into parenthesis
         if balance == 0:
             parenthesis = ''
-            for i in t:
+            for i in b:
                 if i == 0:
                     parenthesis += '('
                 else:
